5e763a88dd87bcf9772881e10f59fd40ec2a4ae2
devb15 <34093122+devb15@users.noreply.github.com>
2024-10-15 10:10:41 +0000

    Added Changes to users

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. Endpoint */
+/* GET users listing. Endpoint Test */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
