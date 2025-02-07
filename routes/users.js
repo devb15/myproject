214e7eaaee11f2e7cf6ce99700008f6e5ea569f6
devb15 <34093122+devb15@users.noreply.github.com>
2025-02-07 16:31:18 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. Endpoint Test */
+/* GET users listing. Endpoint Endpoint Test */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
